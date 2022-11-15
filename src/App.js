039f98152ab4254c6d98ab97568bfdee698cc534
shaonkislo <shaon6340@agmail.com>
2022-11-15 22:06:28 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
+import Inventory from './components/Inventory/Inventory';
+import About from './components/About/About';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>     
+        <Route path='/' element={<Shop></Shop>}></Route>
+        <Route path='/shop' element={<Shop></Shop>}></Route>        
+        <Route path='/orders' element={<Orders/>}></Route>
+        <Route path='/inventory' element={<Inventory/>}></Route>       
+        <Route path='/about' element={<About/>}></Route>       
+      </Routes>
+      
     </div>
   );
 }
